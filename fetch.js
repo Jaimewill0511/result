d8a484f516e907fb2c73f929b1853fadd3fb3795
Jaimewill0511 <67603735+Jaimewill0511@users.noreply.github.com>
2021-04-14 17:00:34 +0100

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -8,7 +8,7 @@ fetch('http://jsonplaceholder.typicode.com/posts')
 .then(res => res.json())
 .then(json =>  {
     let body = JSON.stringify(json, null, 2);
-    fs.writeFile('posts.json', body, (err) => {
+    fs.writeFile('result/posts.json', body, (err) => {
         if(err) throw err;
         //If there is an error
         console.log('file created succesfully')
